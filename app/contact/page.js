0cbfc0863dbde91928f78bc07f38f7bd457e9eca
Muhammad Haseeb <mhaseeb37@gmail.com>
2024-09-12 23:32:05 +0500

    Haseeb - Commits

---
+++ app/contact/page.js
@@ -0,0 +1,7 @@
+export default function Page(){
+    return(
+        <div>
+            <p>Contact Page</p>
+        </div>
+    )
+}
--- app/profile/layout.js
+++ app/profile/layout.js
@@ -1,12 +1,20 @@
+import Header from "@/app/ui/layout/header";
+import Footer from "@/app/ui/layout/footer";
 export default function Layout({ children }){
     return(
-        <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
-            <div className="w-full flex-none md:w-64">
-                Hello
-            </div>
-            <div className="flex-grow p-6 md:overflow-y-auto md:p-12">
-            { children }
+    <>
+        <Header />
+        <div className="bg-white flex h-screen flex-col md:flex-row md:overflow-hidden">
+            <div className="flex h-screen flex-col md:flex-row md:overflow-hidden mt-32">
+                <div className="w-full flex-none md:w-64 text-black">
+                    Hello
+                </div>
+                <div className="flex-grow p-6 md:overflow-y-auto md:p-12">
+                    { children }
+                </div>
             </div>
         </div>
+        <Footer />
+    </>
     )
 }
