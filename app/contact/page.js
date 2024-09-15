e20a57a76569a097d7735f24ea8bff60dd880800
Muhammad Haseeb <mhaseeb37@gmail.com>
2024-09-16 03:53:38 +0500

    Haseeb changes

--- app/contact/page.js
+++ app/contact/page.js
@@ -27,20 +27,33 @@ export default function Page() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     console.log("FormData", formData);
-    const res = await fetch('/api/send-email', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        to: 'mhaseeb37@gmail.com',
-        subject: 'Test Email',
-        message: 'This is a test email sent using Postmark!',
-      }),
-    });
-  
-    const data = await res.json();
-    console.log(data);
+
+    try {
+        const response = await fetch('/api/send-email', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(formData),
+        });
+
+        const result = await response.json();
+        // Reset the form data
+        setFormData({
+            firstName: "",
+            lastName: "",
+            phone: "",
+            to: "",
+            message: "",
+            subject: "General Inquiry",
+          });
+        alert(result.message); // Notify user of success
+
+        
+    } catch (error) {
+        console.error('Error:', error);
+        alert('An error occurred. Please try again.'); // Notify user of error
+    }
 }
   
   return (
