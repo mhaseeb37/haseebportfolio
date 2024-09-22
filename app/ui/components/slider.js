8df534217b6b61effdcf6b56e52f829779fdce28
Muhammad Haseeb <mhaseeb37@gmail.com>
2024-09-23 02:57:15 +0500

    SlideBox with Outline Number

--- app/ui/components/slider.js
+++ app/ui/components/slider.js
@@ -2,6 +2,7 @@ import React from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import Slidebox from "./slidebox";
 
 export default function Sliderslick(props){
     var settings = {
@@ -13,23 +14,23 @@ export default function Sliderslick(props){
       };
     return(
         <Slider {...settings}>
-            <div>
-                <h3>1</h3>
+            <div className="p-10">
+                <Slidebox number={1}/>
             </div>
-            <div>
-                <h3>2</h3>
+            <div className="p-10">
+                <Slidebox number={2}/>
             </div>
-            <div>
-                <h3>3</h3>
+            <div className="p-10">
+                <Slidebox number={3}/> 
             </div>
-            <div>
-                <h3>4</h3>
+            <div className="p-10">
+                <Slidebox number={4}/>
             </div>
-            <div>
-                <h3>5</h3>
+            <div className="p-10">
+                <Slidebox number={5}/>
             </div>
-            <div>
-                <h3>6</h3>
+            <div className="p-10">
+                <Slidebox number={6}/>
             </div>
          </Slider>
     )
