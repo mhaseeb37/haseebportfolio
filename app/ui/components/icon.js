2af345f2205058cc5064ced57a9a3c4bbdb40e23
Muhammad Haseeb <mhaseeb37@gmail.com>
2024-09-24 20:22:05 +0500

    Fixed Issue image src

--- app/ui/components/icon.js
+++ app/ui/components/icon.js
@@ -1,9 +1,10 @@
 import Image from "next/image"
 export default function Icon(props) {
+    const iconSrc = require(`/public/assets/${props.iconname}.png`);
     return(
         <div className="flex items-center justify-center w-12 h-12 p-2 border border-black rounded-full hover:bg-yellow-100">
             <a href={props.url} target="_blank">
-                <Image src={`/assets/${props.iconname}.png`} width={100} height={100} alt={props.iconname}/>
+                <Image src={iconSrc} width={100} height={100} alt={props.iconname}/>
             </a>
         </div>
     )
