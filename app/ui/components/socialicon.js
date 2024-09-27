7be22794817d4265de757458644d2cc3257af0de
Muhammad Haseeb <mhaseeb37@gmail.com>
2024-09-28 01:31:05 +0500

    Icon Colors

--- app/ui/components/socialicon.js
+++ app/ui/components/socialicon.js
@@ -1,9 +1,9 @@
 import Image from "next/image"
 export default function SocialIcon(props) {
     return(
-        <div className="flex items-center justify-center w-12 h-12 p-2 border border-black rounded-full hover:bg-yellow-100">
+        <div className="group flex items-center justify-center w-12 h-12 p-2 border border-white rounded-full hover:bg-white">
             <a href={props.url} target="_blank">
-                <Image src={`/assets/${props.iconname}.png`} width={100} height={100} alt={props.iconname}/>
+                <Image src={`/assets/${props.iconname}.png`} width={100} height={100} alt={props.iconname} className="invert group-hover:invert-0"/>
             </a>
         </div>
     )
