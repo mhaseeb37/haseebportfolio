5da65c21b035242e3ee432192171ed015f68d30e
Muhammad Haseeb <mhaseeb37@gmail.com>
2024-09-30 03:08:49 +0500

    Spaces Fixing.

--- app/ui/components/contactme.js
+++ app/ui/components/contactme.js
@@ -9,7 +9,7 @@ export default function ContactMe() {
     return(
         <div id="contact" className={`${poppins.className} expWrapper pt-10 md:pt-32 w-full md:max-w-3xl lg:max-w-4xl xl:max-w-6xl text-black`}>
             <Titleoverlay overlayText="Contact" subTitle="Let's get in touch" mainTitle="Contact Me"/>
-            <div className="flex justify-start items-center space-x-8 p-4">
+            <div className="flex flex-col md:flex-row justify-start items-start md:items-center md:space-x-8 p-4">
                 <IconBox iconname="phone" url={`tel:${socialLinks.phone}`} data={socialLinks.phone}/>
                 <IconBox iconname="envelope" url={`mailto:${socialLinks.email}`} data={socialLinks.email}/>
                 <IconBox iconname="location" url={`${socialLinks.location}`} data="Lahore, Pakistan"/>
--- app/ui/components/iconbox.js
+++ app/ui/components/iconbox.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function IconBox(props) {
     return(
     
-  <div className="flex items-center space-x-4">
+  <div className="flex items-center space-x-4 mb-4">
     <div className="flex items-center justify-center w-12 h-12 p-2 border border-black rounded-full">
       <Image src={`/assets/${props.iconname}.png`} width={100} height={100} alt={props.iconname}/>
     </div>
