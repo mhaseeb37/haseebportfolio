3d4b55324a0c12671dc6e87adbb7771314af7a0b
Muhammad Haseeb <mhaseeb37@gmail.com>
2024-09-30 03:12:51 +0500

    Social Icons Responsive Fixed

--- app/ui/components/iconbox.js
+++ app/ui/components/iconbox.js
@@ -4,7 +4,7 @@ export default function IconBox(props) {
     return(
     
   <div className="flex items-center space-x-4">
-    <div className="flex items-center justify-center w-12 h-12 p-2 border border-white rounded-full">
+    <div className="flex mb-4 items-start md:items-center justify-center w-12 h-12 p-2 border border-white rounded-full">
       <Image src={`/assets/${props.iconname}.png`} width={100} height={100} alt={props.iconname} className="invert"/>
     </div>
     <p className="text-white">
